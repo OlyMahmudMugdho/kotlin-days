fb56e1b1a158b3f489a56d2d33b5ba01cb98b708
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-03 21:31:36 +0600

    variable declaration with val

--- 02-variable/src/main/kotlin/Main.kt
+++ 02-variable/src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 package com.mahmud
 
-fun main(args : Array<String>) {
-    println("Hello World!")
+fun main() {
+    println(3+2)
+    val age = 23 // variable declaration with val
+    println("my age is $age")
 }
