f1be7f42da0c3fc27d1707b26a39a156fe26e66f
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-03 21:35:39 +0600

    variable declaration with var

--- 02-variable/src/main/kotlin/Main.kt
+++ 02-variable/src/main/kotlin/Main.kt
@@ -1,7 +1,11 @@
 package com.mahmud
 
 fun main() {
-    println(3+2)
     val age = 23 // variable declaration with val
     println("my age is $age")
+
+    var temp = 5
+    println(temp)
+    temp += 8
+    println("value of temp is changed to $temp")
 }
