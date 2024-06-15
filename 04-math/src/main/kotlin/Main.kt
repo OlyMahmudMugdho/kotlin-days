9d22ba08a7e417e8ba2035c63a5dc850b7d2f3e3
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-15 14:18:33 +0600

    ceil

--- 04-math/src/main/kotlin/Main.kt
+++ 04-math/src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 package com.mahmud
 
+import kotlin.math.ceil
 import kotlin.math.floor
 import kotlin.math.round
 import kotlin.math.sqrt
@@ -15,5 +16,8 @@ fun main() {
     println("fraction rounded to ${round(fraction)}")
 
     println("fraction floor to ${floor(fraction)}")
+
+
+    println("fraction ceil to ${ceil(fraction)}")
 
 }
