c2b3ca9373e7ef6d227c3a3d12b043c3777fce90
OlyMahmudMugdho <mugdhodzs38@gmail.com>
2024-06-15 14:13:50 +0600

    sqrt

--- 04-math/src/main/kotlin/Main.kt
+++ 04-math/src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 package com.mahmud
 
+import kotlin.math.sqrt
+
 fun main() {
     println("the value of pi is ${kotlin.math.PI}")
     println("formated and precised ${String.format("%.4f", kotlin.math.PI)}")
+
+    val num = 36F;
+    println("root of $num is ${sqrt(num)}")
 }
